8b0bb33631471dc2fb3938227e84213dc4f6225d
Arno Pan <apan11@my.bcit.ca>
2023-07-20 03:53:34 -0700

    Initial Commit

---
+++ SwiftUIJDMCarInfo/SwiftUIJDMCarInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJDMCarInfoApp.swift
+//  SwiftUIJDMCarInfo
+//
+//  Created by Arno Pan on 2023-07-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJDMCarInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
